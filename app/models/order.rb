48c2f6da9f002b85a12f35c93ea3f5b72d5f7337
Jonas <joaortizro@unal.edu.co>
2017-03-24 01:28:49 -0500

    validations for routes and order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :retailer
   belongs_to :route
+
+  validates :state , :exitDate , :arrivalDate , presence: true
+  validates :totalPrice ,numericality: true
+
 end
